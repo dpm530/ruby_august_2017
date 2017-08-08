75f42ada845e079822746cd202bba036de3a6787
Tyler Thomson <tylerthomson@protonmail.com>
2017-08-08 18:31:29 -0500

    testing

--- tyler_thomson/rails/dojo_dashboard3/config/routes.rb
+++ tyler_thomson/rails/dojo_dashboard3/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'dojos/show/:id' => 'dojos#show'
   get 'dojos/edit/:id' => 'dojos#edit'
   put 'dojos/update/:id' => 'dojos#update'
-  get 'dojos/destroy/:id' => 'dojos#destroy'
+  delete 'dojos/destroy/:id' => 'dojos#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
